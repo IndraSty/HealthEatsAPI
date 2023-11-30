96d4088247eb3c0da2428df1dbf0d4211c443cf6
yudadh <100004095+yudadh@users.noreply.github.com>
2023-11-30 23:32:24 +0800

    Import router dari routes

--- src/application/app.js
+++ src/application/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import router from "../routes/routes.js";
 
 dotenv.config();
 
@@ -8,3 +9,5 @@ export const app = express();
 
 app.use(express.json());
 app.use(cors());
+
+app.use(router)
