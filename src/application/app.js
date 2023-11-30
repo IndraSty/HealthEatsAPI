b04a94c4889ad5e681d1f542d24d20e800392adc
Indoradesu <indrastyawan0925@gmail.com>
2023-12-01 00:59:48 +0700

    update code for authentication

--- src/application/app.js
+++ src/application/app.js
@@ -1,6 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import cookieParser from "cookie-parser";
+import { routes } from "../routes/routes.js";
 
 dotenv.config();
 
@@ -8,3 +10,6 @@ export const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use(cookieParser())
+
+app.use(routes)
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,4 +1,7 @@
 import express from "express";
+import userController from "../controller/user-controller.js";
+import { authMiddleware } from "../middleware/authMiddleware.js";
+import { updateToken } from "../controller/updateToken.js";
 
 
 export const routes = express.Router();
@@ -6,3 +9,9 @@ export const routes = express.Router();
 routes.get('/', (req, res) => {
     res.send("Server Running!")
 })
+
+routes.post('/users/register', userController.createUser);
+routes.post('/users/login', userController.login);
+routes.get('/users/token', updateToken)
+routes.get('/users/current', authMiddleware, userController.getUser);
+routes.delete('/users/logout', authMiddleware, userController.logout);
